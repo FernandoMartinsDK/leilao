c7d540362e90279bbaf2af1a081932c354494fee
Fernando <fernandomartinsdk@gmail.com>
2022-09-30 16:42:04 -0300

    Nova conf docker

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+if [! -f ".env"]; then
+    echo "Criando arquivo .env para $APP_ENV"
+    cp .env.example .env
+else
+    echo "arquivo .env já existe"
+fi
+
+role=${CONTAINER_ROLE:-app}
+
+if [ "$role" = "app" ]; then
+    php artisan migrate
+    php artisan route:clear
+    php artisan route:cache
+    php artisan config:clear
+    php artisan config:cache
+    php artisan view:clear
+    php artisan view:cache
+    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+    exec docker-php-entrypoint "$@"
+elif [ "$role" = "queue" ]; then
+    echo "Running the queue ... "
+    php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
+elif [ "$role" = "websocket" ]; then
+    echo "Running the websocket server ... "
+    php artisan websockets:serve
+fi
