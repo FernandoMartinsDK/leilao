f7278718b7e145a1644640d79291ced81df36c83
FernandoMartinsDk <fernandomartinsdk@gmail.com>
2022-10-07 19:51:40 -0300

    mudança seed

--- database/migrations/2022_10_07_001617_create_auction_items_seed.php
+++ database/migrations/2022_10_07_001617_create_auction_items_seed.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         $this->seedValue = [
             [
-                //"item_id" => "1",
+                "item_id" => "1",
                 "auction_id" => '2',
                 "opening_bid" => "5000",
                 "value_bid" => "5000",
@@ -22,7 +22,106 @@ return new class extends Migration
                 "updated_at" => now()
             ],
             [
-                //"item_id" => "2",
+                "item_id" => "2",
+                "auction_id" => '1',
+                "opening_bid" => "50000",
+                "value_bid" => "50000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "3",
+                "auction_id" => '1',
+                "opening_bid" => "14000",
+                "value_bid" => "14000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "4",
+                "auction_id" => '1',
+                "opening_bid" => "17000",
+                "value_bid" => "17000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "5",
+                "auction_id" => '1',
+                "opening_bid" => "27000",
+                "value_bid" => "27000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "6",
+                "auction_id" => '1',
+                "opening_bid" => "54000",
+                "value_bid" => "54000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "7",
+                "auction_id" => '1',
+                "opening_bid" => "27000",
+                "value_bid" => "27000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "8",
+                "auction_id" => '1',
+                "opening_bid" => "70200",
+                "value_bid" => "70200",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "9",
+                "auction_id" => '1',
+                "opening_bid" => "45000",
+                "value_bid" => "45000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "10",
+                "auction_id" => '1',
+                "opening_bid" => "67000",
+                "value_bid" => "67000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "11",
+                "auction_id" => '1',
+                "opening_bid" => "347000",
+                "value_bid" => "347000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "12",
+                "auction_id" => '1',
+                "opening_bid" => "87000",
+                "value_bid" => "87000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "13",
                 "auction_id" => '1',
                 "opening_bid" => "7000",
                 "value_bid" => "7000",
@@ -31,37 +130,46 @@ return new class extends Migration
                 "updated_at" => now()
             ],
             [
-                //"item_id" => "3",
+                "item_id" => "14",
                 "auction_id" => '1',
-                "opening_bid" => "7000",
-                "value_bid" => "7000",
+                "opening_bid" => "69800",
+                "value_bid" => "69800",
                 "note" => "Segundo Leilão",
                 "created_at" => now(),
                 "updated_at" => now()
             ],
             [
-                //"item_id" => "1",
+                "item_id" => "15",
                 "auction_id" => '1',
-                "opening_bid" => "7000",
-                "value_bid" => "7000",
+                "opening_bid" => "58200",
+                "value_bid" => "58200",
                 "note" => "Segundo Leilão",
                 "created_at" => now(),
                 "updated_at" => now()
             ],
             [
-                //"item_id" => "2",
+                "item_id" => "16",
                 "auction_id" => '1',
-                "opening_bid" => "7000",
-                "value_bid" => "7000",
+                "opening_bid" => "23400",
+                "value_bid" => "23400",
                 "note" => "Segundo Leilão",
                 "created_at" => now(),
                 "updated_at" => now()
             ],
             [
-                //"item_id" => "3",
+                "item_id" => "17",
                 "auction_id" => '1',
-                "opening_bid" => "7000",
-                "value_bid" => "7000",
+                "opening_bid" => "33000",
+                "value_bid" => "33000",
+                "note" => "Segundo Leilão",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "item_id" => "18",
+                "auction_id" => '1',
+                "opening_bid" => "703200",
+                "value_bid" => "703200",
                 "note" => "Segundo Leilão",
                 "created_at" => now(),
                 "updated_at" => now()
